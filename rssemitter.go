0ae36c184a79f0db401462956c86f127d65fb88c
Kentoy <jankenith.johannessen@gmail.com>
2017-11-11 23:32:23 +0800

    added implementation of interfaces, needs testing :poop:

--- rssemitter.go
+++ rssemitter.go
@@ -13,3 +13,45 @@ type FeedEmitter interface {
 	Remove(gofeed.Feed) void
 	Destroy() void
 }
+
+type RssFeedEmitter struct {
+	feeds []gofeed.Feed
+	parser gofeed.Parser
+}
+
+func NewFeedEmitter() *RssFeedEmitter {
+	feedEmitter := RssFeedEmitter{
+		parser: gofeed.NewParser()
+	}
+	return feedEmitter
+}
+
+func (r RssFeedEmitter) Add(url string) {
+	for _, feed := range r.feeds {
+		if feed.FeedLink == url {
+			return
+		}
+	}
+	newFeed, err := r.parser.ParseURL(url)
+	if err != nil {
+		return
+	}
+	r.feeds = append(r.feeds, newFeed)
+}
+
+func (r RssFeedEmitter) List() *[]gofeed.Feed {
+	return r.feeds
+}
+
+func (r RssFeedEmitter) Remove(url string) {
+	for i = 0; i < len(r.feeds); i++ {
+		if r.feeds[i].FeedLink = url {
+			r.feeds = append(r.feeds[:i], a[i+1:]...)
+		}
+	}
+}
+
+func (r RssFeedEmitter) Destroy() *RssFeedEmitter{
+	r = NewFeedEmitter()
+	return r
+}
