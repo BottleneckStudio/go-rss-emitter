2014ff2f6d2bccbc21f03b9b3e7aa9d8dd436ca6
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2017-11-12 19:33:32 +0800

    Add structs (#4)
    * added dependency and interface
    
    * added implementation of interfaces, needs testing :poop:
    
    * removed main completely
    
    * added comments

--- rssemitter.go
+++ rssemitter.go
@@ -1,27 +1,65 @@
 package rssemitter
 
 import (
-	"fmt"
+	// "fmt"
 
-	"github.com/chuckpreslar/emission"
+	// "github.com/chuckpreslar/emission"
+	"github.com/mmcdole/gofeed"
 )
 
-func main() {
-	// fmt.Println("Hello World")
-	emitter := emission.NewEmitter()
+// FeedEmitter is our public interface
+type FeedEmitter interface {
+	Add(gofeed.Feed)
+	List() []gofeed.Feed
+	Remove(gofeed.Feed)
+	Destroy()
+}
 
-	hello := func(to string) {
-		fmt.Printf("Hello %s!\n", to)
+// RssFeedEmitter is the base model for our plugin
+type RssFeedEmitter struct {
+	feeds  []gofeed.Feed
+	parser *gofeed.Parser
+}
+
+// NewFeedEmitter creates a new instance of RssFeedEmitter
+func NewFeedEmitter() *RssFeedEmitter {
+	feedEmitter := RssFeedEmitter{
+		parser: gofeed.NewParser(),
 	}
+	return &feedEmitter
+}
 
-	count := func(count int) {
-		for i := 0; i < count; i++ {
-			fmt.Println(i)
+// Add method expects a url string and adds it to the
+// list of feeds in the RssFeedEmitter
+func (r RssFeedEmitter) Add(url string) {
+	for _, feed := range r.feeds {
+		if feed.FeedLink == url {
+			return
 		}
 	}
+	newFeed, err := r.parser.ParseURL(url)
+	if err != nil {
+		return
+	}
+	r.feeds = append(r.feeds, *newFeed)
+}
 
-	emitter.On("hello", hello).
-		On("count", count).
-		Emit("hello", "world").
-		Emit("count", 5)
+// List returns the current list of feeds
+func (r RssFeedEmitter) List() *[]gofeed.Feed {
+	return &r.feeds
+}
+
+// Remove expects a string and removes the feed from the list
+func (r RssFeedEmitter) Remove(url string) {
+	for i := 0; i < len(r.feeds); i++ {
+		if r.feeds[i].FeedLink == url {
+			r.feeds = append(r.feeds[:i], r.feeds[i+1:]...)
+		}
+	}
+}
+
+// Destroy clears the feed emitter
+func (r RssFeedEmitter) Destroy() *RssFeedEmitter {
+	r = *NewFeedEmitter()
+	return &r
 }
