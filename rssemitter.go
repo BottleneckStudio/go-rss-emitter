d0fa264b254f525e5923f0b878f6a31fcd022e07
Kentoy <jankenith.johannessen@gmail.com>
2017-11-12 12:34:28 +0800

    added comments

--- rssemitter.go
+++ rssemitter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
+// FeedEmitter is our public interface
 type FeedEmitter interface {
 	Add(gofeed.Feed)
 	List() []gofeed.Feed
@@ -14,11 +15,13 @@ type FeedEmitter interface {
 	Destroy()
 }
 
+// RssFeedEmitter is the base model for our plugin
 type RssFeedEmitter struct {
 	feeds  []gofeed.Feed
 	parser *gofeed.Parser
 }
 
+// NewFeedEmitter creates a new instance of RssFeedEmitter
 func NewFeedEmitter() *RssFeedEmitter {
 	feedEmitter := RssFeedEmitter{
 		parser: gofeed.NewParser(),
@@ -26,6 +29,8 @@ func NewFeedEmitter() *RssFeedEmitter {
 	return &feedEmitter
 }
 
+// Add method expects a url string and adds it to the
+// list of feeds in the RssFeedEmitter
 func (r RssFeedEmitter) Add(url string) {
 	for _, feed := range r.feeds {
 		if feed.FeedLink == url {
@@ -39,10 +44,12 @@ func (r RssFeedEmitter) Add(url string) {
 	r.feeds = append(r.feeds, *newFeed)
 }
 
+// List returns the current list of feeds
 func (r RssFeedEmitter) List() *[]gofeed.Feed {
 	return &r.feeds
 }
 
+// Remove expects a string and removes the feed from the list
 func (r RssFeedEmitter) Remove(url string) {
 	for i := 0; i < len(r.feeds); i++ {
 		if r.feeds[i].FeedLink == url {
@@ -51,6 +58,7 @@ func (r RssFeedEmitter) Remove(url string) {
 	}
 }
 
+// Destroy clears the feed emitter
 func (r RssFeedEmitter) Destroy() *RssFeedEmitter {
 	r = *NewFeedEmitter()
 	return &r
